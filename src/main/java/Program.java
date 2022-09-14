72b917d865411caca5253adc28e2b40372bb60f8
Roj Mert Tekin <rojt@net.chalmers.se>
2022-09-14 13:25:56 +0200

    setWindowTitleText method created

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,7 +3,14 @@ import de.gurkenlabs.litiengine.Game;
 public class Program {
 
     public static void main(String[] args) {
+        setWindowTitleText();
         Game.init(args);
         Game.start();
+    }
+
+    private static void setWindowTitleText() {
+        Game.info().setName("hej");
+        Game.info().setVersion("0.01");
+        Game.info().setSubTitle("");
     }
 }

f0b6e07aa04a43e50c1d1374e4ccf27fe2d56469
Elias Haqiqat <elias.haqiqat@protonmail.com>
2022-09-15 12:23:30 +0200

    Add resource folders and basic game info

--- src/main/java/Controller/Program.java
+++ src/main/java/Controller/Program.java
@@ -1,18 +1,33 @@
 package Controller;
 
+import View.IngameScreen;
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.resources.Resources;
 
 public class Program {
 
     public static void main(String[] args) {
         setWindowTitleText();
         Game.init(args);
+        setWindowData();
+        loadResources();
+        Game.world().loadEnvironment("TestMap");
+        Game.screens().add(new IngameScreen());
         Game.start();
+
     }
 
     private static void setWindowTitleText() {
         Game.info().setName("hej");
         Game.info().setVersion("0.01");
         Game.info().setSubTitle("");
+    }
+
+    private static void setWindowData() {
+        Game.graphics().setBaseRenderScale(4f);
+    }
+
+    private static void loadResources() {
+        Resources.load("game.litidata");
     }
 }

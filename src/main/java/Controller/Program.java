18d008af368b8ed7797e75d07601400df66b7f54
augnor <augnor@net.chalmers.se>
2022-09-19 11:08:33 +0200

    commit

--- src/main/java/Controller/Program.java
+++ src/main/java/Controller/Program.java
@@ -1,12 +1,18 @@
 package Controller;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.resources.Resources;
+import View.IngameScreen;
 
 public class Program {
 
     public static void main(String[] args) {
         setWindowTitleText();
         Game.init(args);
+        // load datafrom gamefile
+        Resources.load("game.litidata");
+        Game.world().loadEnvironment("Test2ActualMap2");
+        Game.screens().add(new IngameScreen());
         Game.start();
     }
 
---
+++ src/main/java/View/IngameScreen.java
@@ -0,0 +1,11 @@
+package View;
+
+import de.gurkenlabs.litiengine.gui.screens.GameScreen;
+
+public class IngameScreen extends GameScreen {
+    public static final String NAME = "INGAME-SCREEN";
+
+    public IngameScreen(){
+        super(NAME);
+    }
+}

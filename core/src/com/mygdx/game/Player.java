fbae7d01b0a487b3b2bc1dd0deb133cec138b985
pkasinski <kasinski@student.chalmers.se>
2022-09-19 17:08:53 +0200

    Create player class

---
+++ core/src/com/mygdx/game/Player.java
@@ -0,0 +1,39 @@
+package com.mygdx.game;
+
+public class Player {
+private int x;
+private int y;
+private int height;
+private int width;
+
+    public Player(int x, int y, int height, int width) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}

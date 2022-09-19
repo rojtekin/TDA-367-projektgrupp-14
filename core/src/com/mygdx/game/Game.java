2d8784d6d7f355869f9d78a1f577a166e77c5180
pkasinski <kasinski@student.chalmers.se>
2022-09-19 17:09:05 +0200

    Render the player

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -11,22 +11,29 @@ public class Game extends ApplicationAdapter {
 	private Texture playerImage;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	
+	private Player player;
+	private static final int SCREEN_WIDTH = 800;
+	private static final int SCREEN_HEIGHT = 480;
+
 	@Override
 	public void create () {
 		// Load an image
 		playerImage = new Texture(Gdx.files.internal("characters/BlueSamurai-Idle.png"));
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 		batch = new SpriteBatch();
+		player = new Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 32, 32);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 0.9f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		batch.draw(playerImage, player.getX(), player.getY());
 		batch.end();
 	}
 	

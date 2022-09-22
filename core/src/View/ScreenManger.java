044dafc7836c2839cd2885dd760dcb78370aa37f
Abdullah <lolo----995@hotmail.com>
2022-09-22 23:39:13 +0200

    Added some files relating to screens.

---
+++ core/src/View/ScreenManger.java
@@ -0,0 +1,12 @@
+package View;
+
+import com.mygdx.game.Game;
+
+public class ScreenManger {
+    private final Game game;
+
+    public ScreenManger(Game game) {
+        this.game = game;
+    }
+}
+

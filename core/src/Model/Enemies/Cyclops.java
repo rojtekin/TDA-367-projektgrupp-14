8545ca0d3558288417378a5d9e9fa4a87dce7e63
pkasinski <kasinski@student.chalmers.se>
2022-09-29 11:44:37 +0200

    Create the class Cyclops

---
+++ core/src/Model/Enemies/Cyclops.java
@@ -0,0 +1,10 @@
+package Model.Enemies;
+
+import Model.Enemy;
+
+public class Cyclops extends Enemy {
+
+    public Cyclops(int x, int y, int speed, float health, float damage) {
+        super(x, y, 32, 32, speed, health, damage);
+    }
+}

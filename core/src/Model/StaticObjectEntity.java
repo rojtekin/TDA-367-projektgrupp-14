b3c15bd201281b5a0675c053d698ecdedbbbf23c
Elias Haqiqat <elias.haqiqat@protonmail.com>
2022-09-29 14:27:19 +0200

    Add documentation to and renamed StaticObjectEntity.java

--- core/src/Model/StaticObjectEntity.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-import com.dongbat.jbump.World;
-
-public class StaticObjectEntity extends Entity {
-    public StaticObjectEntity(float x, float y, float height, float width, World<Entity> world) {
-        super(x, y, height, width, 0, 1000000, world);
-    }
-}

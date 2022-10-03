c45908cc8ee3784c9c7eed3b5866855ae1ad71c9
pkasinski <kasinski@student.chalmers.se>
2022-10-03 15:18:36 +0200

    Add instances of two different enemies

--- core/src/Model/ModelFactory.java
+++ core/src/Model/ModelFactory.java
@@ -1,8 +1,15 @@
 package Model;
 
+import Model.Enemies.Cyclops;
+import Model.Enemies.Mouse;
+
 public class ModelFactory {
     public static Model makeModel(String mapName) {
         Model model = new Model();
+        model.addEnemy(new Cyclops(100, 100, 1, 1, 1, model.getWorld()));
+        model.addEnemy(new Cyclops(100, 200, 1, 1, 1, model.getWorld()));
+        model.addEnemy(new Cyclops(200, 100, 1, 1, 1, model.getWorld()));
+        model.addEnemy(new Mouse(200, 200,2, 1, 1, model.getWorld()));
         model.initialize(mapName);
         return model;
     }

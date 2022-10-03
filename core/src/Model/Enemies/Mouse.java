215eb0a9bbafc5ce7a3139e54be3e4898dfef5f2
pkasinski <kasinski@student.chalmers.se>
2022-10-03 15:17:45 +0200

    Set the height and width of a mouse to 16

--- core/src/Model/Enemies/Mouse.java
+++ core/src/Model/Enemies/Mouse.java
@@ -5,8 +5,8 @@ import com.dongbat.jbump.World;
 
 public class Mouse extends Enemy {
 
-    public Mouse(float x, float y, float height, float width, float speed, float health, float damage, World<Entity> world) {
-        super(x, y, height, width, speed, health, damage, world, "Mouse");
+    public Mouse(float x, float y, float speed, float health, float damage, World<Entity> world) {
+        super(x, y, 16, 16, speed, health, damage, world, "Mouse");
     }
 
 }

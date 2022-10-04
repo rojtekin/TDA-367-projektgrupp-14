9b181d123351d64a2275e4a2b68bed6bd0207bac
pkasinski <kasinski@student.chalmers.se>
2022-10-04 15:10:07 +0200

    Make mice move in a different manner

--- core/src/Model/Enemies/Cyclops.java
+++ core/src/Model/Enemies/Cyclops.java
@@ -4,8 +4,51 @@ import Model.Entity;
 import com.dongbat.jbump.World;
 
 public class Cyclops extends Enemy {
+    private boolean stuckInXDirection = false;
+    private boolean stuckInYDirection = false;
 
     public Cyclops(int x, int y, int speed, float health, float damage, World<Entity> world) {
         super(x, y, 32, 32, speed, health, damage, world,"Cyclops");
+    }
+
+    /**
+     * Moves the cyclops toward the player character in the x-direction or in the y-direction.
+     * If the cyclops gets stuck in the x-direction, it is moved up until it is no longer stuck.
+     * If it gets stuck in the y-direction, it is moved right until it is no longer stuck.
+     * @param playerX the x-coordinate of the player character.
+     * @param playerY the y-coordinate of the player character.
+     */
+    public void moveTowardPlayer(float playerX, float playerY) {
+        float xDistance = playerX - this.getX();
+        float yDistance = playerY - this.getY();
+        float initialX = this.getX();
+        float initialY = this.getY();
+
+        if ((Math.abs(xDistance) > 16) && !stuckInYDirection) {
+            if (xDistance > 0) {
+                this.moveRight();
+            }
+            else {
+                this.moveLeft();
+            }
+            stuckInXDirection = false;
+            if (this.getX() == initialX) {
+                stuckInXDirection = true;
+                this.moveUp();
+            }
+        }
+        else if(!stuckInXDirection){
+            if (yDistance > 0) {
+                this.moveUp();
+            }
+            else {
+                this.moveDown();
+            }
+            stuckInYDirection = false;
+            if (this.getY() == initialY) {
+                stuckInYDirection = true;
+                this.moveRight();
+            }
+        }
     }
 }
--- core/src/Model/Enemies/Mouse.java
+++ core/src/Model/Enemies/Mouse.java
@@ -1,12 +1,49 @@
 package Model.Enemies;
 
 import Model.Entity;
+import com.badlogic.gdx.Gdx;
 import com.dongbat.jbump.World;
 
 public class Mouse extends Enemy {
+    private float timeSinceDirectionChanged = 0;
 
     public Mouse(float x, float y, float speed, float health, float damage, World<Entity> world) {
         super(x, y, 16, 16, speed, health, damage, world, "Mouse");
     }
 
+
+    /**
+     * Moves the mouse in the direction it is facing. After a certain amount of time since changing the direction has
+     * passed, the direction of the mouse is randomly changed, so it moves toward the player character
+     * in the x-direction or y-direction.
+     * @param playerX the x-coordinate of the player character.
+     * @param playerY the y-coordinate of the player character.
+     */
+    @Override
+    public void moveTowardPlayer(float playerX, float playerY) {
+        float xDistance = playerX - this.getX();
+        float yDistance = playerY - this.getY();
+        timeSinceDirectionChanged += Gdx.graphics.getDeltaTime();
+
+        if (timeSinceDirectionChanged > 0.6) {
+            if (Math.random() < 0.5) {
+                if ((xDistance > 0)) {
+                    this.moveRight();
+                } else {
+                    this.moveLeft();
+                }
+            }
+            else {
+                if ((yDistance > 0)) {
+                    this.moveUp();
+                } else {
+                    this.moveDown();
+                }
+            }
+            timeSinceDirectionChanged = 0;
+        }
+        else {
+            this.move();
+        }
+    }
 }

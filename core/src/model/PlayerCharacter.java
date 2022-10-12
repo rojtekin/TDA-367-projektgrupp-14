ebdb2911e33913a923ad0a7f42c1fc221fab1339
Elias Haqiqat <elias.haqiqat@protonmail.com>
2022-10-12 16:49:59 +0200

    Added alternative constructors

--- core/src/model/PlayerCharacter.java
+++ core/src/model/PlayerCharacter.java
@@ -4,8 +4,19 @@ import com.dongbat.jbump.World;
 
 public class PlayerCharacter extends LivingEntity implements IControllable {
 
+    private static final String DEFAULTFACTION = "player";
+
     public PlayerCharacter(float spawnX, float spawnY, World<Entity> world) {
-        super(spawnX, spawnY, 32, 32, 5, 10, 0, "Friendly", world);
+        super(spawnX, spawnY, 32, 32, 5, 10, 0, DEFAULTFACTION, world);
+    }
+
+    /**
+     * Alternative constructor that allows a player to be of another faction.
+     * Potential use in multiplayer
+     * @param faction that the player belongs to
+     */
+    public PlayerCharacter(float spawnX, float spawnY, String faction, World<Entity> world) {
+        super(spawnX, spawnY, 32, 32, 5, 10, 0, faction, world);
     }
 
     @Override
--- core/src/model/monsters/Monster.java
+++ core/src/model/monsters/Monster.java
@@ -6,10 +6,21 @@ import com.dongbat.jbump.World;
 import model.IDamageVisitor;
 
 public abstract class Monster extends LivingEntity {
+    //Bad practice? only works if static
+    private static final String DEFAULTFACTION = "hostile";
     private double damage;
 
     public Monster(float x, float y, float height, float width, float speed, float health, float damage, World<Entity> world) {
-        super(x, y, height, width, speed, health, 1, "Hostile", world);
+        super(x, y, height, width, speed, health, 1, DEFAULTFACTION, world);
+        this.damage = damage;
+    }
+
+    /**
+     * Alternative constructor that allows custom factions
+     * @param faction Alternative faction
+     */
+    public Monster(float x, float y, float height, float width, float speed, float health, float damage, String faction, World<Entity> world) {
+        super(x, y, height, width, speed, health, 1, faction, world);
         this.damage = damage;
     }
 

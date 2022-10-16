c850b48cc228e59d6c8fdac93a45689efe3c2421
rojtekin <rojt@net.chalmers.se>
2022-10-16 15:12:19 +0200

    Idle sound from entities now plays diffrent sounds depending on entities and volume based on distance. Functionality for enemy hit sound and players sword attack sound created.

--- core/src/view/Sound.java
+++ core/src/view/Sound.java
@@ -1,45 +1,57 @@
 package view;
 
-import model.enemies.Mouse;
 import model.Entity;
 import model.Model;
 import com.badlogic.gdx.Gdx;
 
 public class Sound{
-    private final String backgroundMusicPath = "Audio/Background music.ogg";
+    private final com.badlogic.gdx.audio.Music backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("Audio/Background music.ogg"));
+    private final com.badlogic.gdx.audio.Sound enemyHit = Gdx.audio.newSound(Gdx.files.internal("Audio/enemyHit.mp3"));
+    private final com.badlogic.gdx.audio.Sound swordSwoosh = Gdx.audio.newSound(Gdx.files.internal("Audio/sword-swoosh.mp3"));
 
-    public void initialize(){
-        com.badlogic.gdx.audio.Music backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal(backgroundMusicPath));
+    public void playGameMusic(){
         backgroundMusic.setLooping(true);
-       // backgroundMusic.play();
+        backgroundMusic.play();
         backgroundMusic.setVolume(0.1f);
     }
 
-    public com.badlogic.gdx.audio.Sound getSound() { //TODO generalise for everything
+    public void stopGameMusic(){
+        backgroundMusic.dispose();
+    }
 
-        com.badlogic.gdx.audio.Sound sound = Gdx.audio.newSound(Gdx.files.internal("Audio/Mouse-squeek.mp3"));
+    public void playEnemyHit(){
+        enemyHit.play(); // TODO: check if the sounds isnt too loud later
+    }
+
+    public  void playSwordHit(){
+        swordSwoosh.play(); // TODO: check if the sounds isnt too loud later
+    }
+
+    public com.badlogic.gdx.audio.Sound getIdleSound(Entity entity) {
+        com.badlogic.gdx.audio.Sound sound = Gdx.audio.newSound(Gdx.files.internal("Audio/"+ entity.getClass().getSimpleName() +"-idle-noise.mp3"));
         return sound;
     }
 
-    public void playSounds(final Model model) {
-        // occasionally runs the function getSound() if its entity exists. timer starts when entity "spawns".
+    public void playIdleSoundsWithInterval(final Model model, long interval) {
+        // occasionally runs the function getIdleSound() and plays the sound if its entity exists. timer starts when entity "spawns".
         for (final Entity entity : model.getEntities()) {
             Runnable toRun = new Runnable() {
-                    public void run() {
-                        while (model.getEntities().contains(entity)){
-                            float distance = (float) Math.hypot(entity.getX()-model.getPlayer().getX(), entity.getY()-model.getPlayer().getY());
-                            distance = 100/distance;
-                            getSound().play(distance);
-                            try {
-                                Thread.sleep(2000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
+                public void run() {
+                    while (model.getEntities().contains(entity)){
+                        float distance = (float) Math.hypot(entity.getX()-model.getPlayer().getX(), entity.getY()-model.getPlayer().getY());
+                        distance = 300/(distance*entity.getWidth());
+                        getIdleSound(entity).play(distance);
+                        try {
+                            Thread.sleep(interval);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
                     }
-                };
-                new Thread(toRun).start();
-            }
+                }
+            };
+            new Thread(toRun).start();
+
         }
+    }
 
 }

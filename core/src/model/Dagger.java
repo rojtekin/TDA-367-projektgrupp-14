45d398a965d272ebbc19eef3e525ba9a9981a8a5
Suedie <elias.haqiqat@protonmail.com>
2022-10-19 01:56:09 +0200

    Improved hit detection but still very buggy

--- core/src/model/Dagger.java
+++ core/src/model/Dagger.java
@@ -1,11 +1,9 @@
 package model;
 
-import com.dongbat.jbump.CollisionFilter;
-import com.dongbat.jbump.Item;
-import com.dongbat.jbump.Response;
-import com.dongbat.jbump.World;
+import com.dongbat.jbump.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Dagger implements IWeapon {
 
@@ -16,19 +14,41 @@ public class Dagger implements IWeapon {
         this.collisionWorld = world;
     }
 
-    public void attack(float x, float y, float playerDamage, Faction faction, Direction dir) {
-        int xdir = 1;
-        int ydir = 1;
-        if (dir.x <= 0) {
-            xdir = -1;
+    public void attack(float initX, float initY, float playerDamage, Faction faction, Direction dir) {
+        Item<IEntity> hurtBox = new Item<>(null);
+        Response.Result result;
+        float x;
+        float width;
+        float y;
+        float height;
+        if (dir == Direction.DOWN) {
+            x = initX + 8; width = initX + 24; y = initY; height = initY - 10;
+            collisionWorld.add(hurtBox,x, y, width, height);
+            result = collisionWorld.check(hurtBox, x, y, crossFilter());
+            collisionHelper(result.projectedCollisions, playerDamage, faction);
+        } else if (dir == Direction.UP) {
+            x = initX + 8; y = initY + 32; width = initX + 24; height = initY + 42;
+            collisionWorld.add(hurtBox,x, y, width, height);
+            result = collisionWorld.check(hurtBox, x, y, crossFilter());
+            collisionHelper(result.projectedCollisions, playerDamage, faction);
+        } else if (dir == Direction.RIGHT) {
+            x = initX + 32; y = initY + 8; width = initX + 42; height = initY + 24;
+            collisionWorld.add(hurtBox,x, y, width, height);
+            result = collisionWorld.check(hurtBox, x, y, crossFilter());
+            collisionHelper(result.projectedCollisions, playerDamage, faction);
+        } else if (dir == Direction.LEFT) {
+            x = initX; y = initY + 8; width = initX -10; height = initY + 24;
+            collisionWorld.add(hurtBox,x, y, width, height);
+            result = collisionWorld.check(hurtBox, x, y, crossFilter());
+            collisionHelper(result.projectedCollisions, playerDamage, faction);
         }
-        if (dir.y <= 0) {
-            ydir = -1;
-        }
-        ArrayList<Item> hit = new ArrayList<>();
-        collisionWorld.queryRect(x, y, x + (xdir*20), y + (ydir*10), crossFilter(), hit);
-        for (Item<Entity> i : hit) {
-            i.userData.beAttacked(playerDamage + weaponDamage, faction);
+        collisionWorld.remove(hurtBox);
+    }
+
+    private void collisionHelper(Collisions collisions, float playerDamage, Faction faction) {
+        for (int i = 0; i < collisions.size(); i++) {
+            Item<Entity> touched = collisions.get(i).other;
+            touched.userData.beAttacked(playerDamage + weaponDamage, faction);
         }
     }
 

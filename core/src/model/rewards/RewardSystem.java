568ab6e8d31fe754b8d32f86d9b4da505c71b549
rojtekin <rojt@net.chalmers.se>
2022-10-18 11:34:26 +0200

    Added documentation

--- core/src/model/rewards/RewardSystem.java
+++ core/src/model/rewards/RewardSystem.java
@@ -8,17 +8,32 @@ import model.playerperks.SpeedDevil;
 import model.playerperks.Tank;
 
 public class RewardSystem {
+    private static final int EXPERIENCE_THRESHOLD = 100;
     private boolean perkApplied;
     private World<IEntity> world;
 
+    /**
+     * Initializes the RewardSystem class so that a perk can be applied
+     * @param world the world it initializes on.
+     */
     public void initialize(World<IEntity> world) {
         perkApplied = false;
         this.world = world;
     }
+
+    /**
+     * checks if the playerCharacter has enough experience to level up
+     * @param playerCharacter the playerCharacter that holds experience
+     * @return true if experience is higher than or equals EXPERIENCE_THRESHOLD otherwise false
+     */
     public boolean levelUpChecker(IPlayerCharacter playerCharacter){
-        return playerCharacter.getExperience() >= 100;
+        return playerCharacter.getExperience() >= EXPERIENCE_THRESHOLD;
     }
 
+    /**
+     * Generates a random level up reward, if a perk is already applied then the reward can't be a perk
+     * @return a randomised reward
+     */
     public Reward getRandomReward(){
         Reward reward = Reward.randomReward();
         if ((reward == Reward.TANK && perkApplied )||(reward == Reward.GLASS_CANNON && perkApplied) || (reward == Reward.SPEED_DEVIL && perkApplied)){
@@ -27,6 +42,12 @@ public class RewardSystem {
         return reward;
     }
 
+    /**
+     * Applies a reward on a PlayerCharacter
+     * @param playerCharacter the playerCharacter it gets applied to
+     * @param reward the reward that gets applied
+     * @return the resulting PlayerCharacter with the reward applied
+     */
     public IPlayerCharacter applyReward(IPlayerCharacter playerCharacter, Reward reward){
         playerCharacter.reduceExperience();
         if (reward == Reward.DAMAGE_INCREASE){

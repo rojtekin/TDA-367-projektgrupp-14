d2de5d37cccae7b854902e4531e4333123cdf912
rojtekin <rojt@net.chalmers.se>
2022-10-18 10:50:30 +0200

    Added documentation

--- core/src/view/Sound.java
+++ core/src/view/Sound.java
@@ -10,24 +10,39 @@ public class Sound{
     private static final com.badlogic.gdx.audio.Sound SWORD_SWOOSH = Gdx.audio.newSound(Gdx.files.internal("Audio/sword-swoosh.mp3"));
     private static final com.badlogic.gdx.audio.Sound PLAYER_DEATH = Gdx.audio.newSound(Gdx.files.internal("Audio/player-death.mp3"));
 
+    /**
+     * Plays the background music of the game, constantly looping.
+     */
     public void playGameMusic(){
         BACKGROUND_MUSIC.setLooping(true);
         BACKGROUND_MUSIC.play();
         BACKGROUND_MUSIC.setVolume(0.1f);
     }
 
+    /**
+     * Stops playing the background music.
+     */
     public void stopGameMusic(){
         BACKGROUND_MUSIC.dispose();
     }
 
+    /**
+     * Plays the sound associated with the enemy hitting something/someone.
+     */
     public void playEnemyHit(){
         ENEMY_HIT.play(); // TODO: check if the sounds isnt too loud later
     }
 
+    /**
+     * Plays the sound of the sword swing.
+     */
     public  void playSwordHit(){
         SWORD_SWOOSH.play(); // TODO: check if the sounds isnt too loud later
     }
 
+    /**
+     * Plays the player death sound.
+     */
     public void playPlayerDeathSound(){
         PLAYER_DEATH.play();
     }
@@ -37,8 +52,12 @@ public class Sound{
         return sound;
     }
 
+    /**
+     * Continuously plays the idle-sounds of every living entity in the model.
+     * @param model a reference to the model the sounds are associated with.
+     * @param interval how long between each play of the idle sounds.
+     */
     public void playIdleSoundsWithInterval(final Model model, long interval) {
-        // occasionally runs the function getIdleSound() and plays the sound if its entity exists. timer starts when entity "spawns".
         for (final Entity entity : model.getEntities()) {
             Runnable toRun = new Runnable() {
                 public void run() {

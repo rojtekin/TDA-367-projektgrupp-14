0259f9934b0753de98c4e1d1fdeb209ea3e70c2c
Elias Haqiqat <elias.haqiqat@protonmail.com>
2022-10-22 16:36:44 +0200

    Add MapTest

---
+++ core/test/java/MapTest.java
@@ -0,0 +1,31 @@
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import model.Model;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.Gdx.*;
+import model.TiledMapCache;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MapTest {
+
+    TiledMapCache tmc;
+
+    @BeforeEach
+    public void setUp() {
+        tmc = new TiledMapCache();
+    }
+
+    @Test
+    public void TiledMapCache_LoadsDefaultValues() {
+        assertTrue(tmc.getMapUnitHeight() == 0 && tmc.getMapUnitWidth() == 0 &&
+                tmc.getSpawnPoints().size() == 8);
+    }
+
+    //@Test
+    //public void loadEnvironment_LoadsMap() {
+    //    tmc.loadEnvironment("Flowerfield");
+    //    assertTrue(tmc.getMap() != null);
+    //}
+}

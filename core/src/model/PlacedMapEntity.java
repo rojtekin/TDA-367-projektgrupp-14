c18637e1f24aa8a19c0c05cf0e6e10a8cf2bce9b
Elias Haqiqat <elias.haqiqat@protonmail.com>
2022-10-23 16:12:55 +0200

    Add javadoc

--- core/src/model/PlacedMapEntity.java
+++ core/src/model/PlacedMapEntity.java
@@ -8,6 +8,14 @@ import com.dongbat.jbump.World;
  * world that the player cannot directly interact with, such as walls.
  */
 public class PlacedMapEntity extends Entity {
+    /**
+     * Creates a static entity on the map that has collision
+     * @param x the x position
+     * @param y the y position
+     * @param height its height
+     * @param width its width
+     * @param world the JBump world where the collisionbox exists
+     */
     public PlacedMapEntity(float x, float y, float height, float width, World<IEntity> world) {
         super(x, y, height, width, 0, world);
     }

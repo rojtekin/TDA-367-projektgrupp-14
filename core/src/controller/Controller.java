15c097a668395405b5f695121ebadd18b17ec8e3
pkasinski <kasinski@student.chalmers.se>
2022-10-23 11:59:51 +0200

    Refactor code in Controller.java

--- core/src/controller/Controller.java
+++ core/src/controller/Controller.java
@@ -10,54 +10,52 @@ import java.util.Objects;
  * A class responsible for updating the model in response to user input.
  */
 public class Controller {
-    private Model model;
-    private IPlayerCharacter player;
+    private final IPlayerCharacter player;
 
-    public Controller(Model model) {
-        this.model = Objects.requireNonNull(model);
-        this.player = Objects.requireNonNull(model.getPlayer());
+    public Controller(IPlayerCharacter player) {
+        this.player = Objects.requireNonNull(player);
     }
 
-    public void update(IPlayerCharacter player) {
-        model.getPlayer().setMoving(false);
+    public void update() {
+        player.setMoving(false);
         // User input
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
             player.move(Direction.LEFT);
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
             player.move(Direction.RIGHT);
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
             player.move(Direction.UP);
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
             player.move(Direction.DOWN);
         }
 
-        model.getPlayer().setSwinging(false);
+        player.setSwinging(false);
         // sword attack
 
         //Left
         if(Gdx.input.isKeyJustPressed(Input.Keys.L)){
             player.weaponAttack(315,45,0);
-            model.getPlayer().setSwinging(true);
+            player.setSwinging(true);
 
         }
         //Up
         if(Gdx.input.isKeyJustPressed(Input.Keys.K)){
             player.weaponAttack(225,315,0);
-            model.getPlayer().setSwinging(true);
+            player.setSwinging(true);
 
         }
         //right
         if(Gdx.input.isKeyJustPressed(Input.Keys.J)){
             player.weaponAttack(135,225,0);
-            model.getPlayer().setSwinging(true);
+            player.setSwinging(true);
         }
         //Down
         if(Gdx.input.isKeyJustPressed(Input.Keys.I)){
             player.weaponAttack(45,135,0);
-            model.getPlayer().setSwinging(true);
+            player.setSwinging(true);
         }
     }
 }
